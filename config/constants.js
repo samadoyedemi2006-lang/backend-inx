9c233130c455168bc501488eda99c139fee84fb7
samadoyedemi2006-lang <samadoyedemi@gmail.com>
2026-03-03 16:09:29 -0600

    Re

--- config/constants.js
+++ config/constants.js
@@ -4,7 +4,7 @@ export default {
   DAILY_ROI_PERCENT: 15,
   ROI_TOTAL_DAYS: 5,
   ROI_INTERVAL_MS: 60 * 60 * 1000, // 10 minutes for testing
-  MIN_WITHDRAWAL: 4000,
+  MIN_WITHDRAWAL: 3700,
   PLANS: {
     starter: { name: "Starter Growth", amount: 2000 },
     silver: { name: "Silver Growth", amount: 5000 },
